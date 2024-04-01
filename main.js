2da71771d9f56d5b68280285b1471407a7129c39
saliffarhan <161368049+saliffarhan@users.noreply.github.com>
2024-04-02 01:11:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter your first value", type: "number", name: "num1" },
+    { message: "Enter your Second value", type: "number", name: "num2" },
+    {
+        message: "Select your correct operator: ",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Divide",],
+    },
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.num1 + answer.num2);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.num1 - answer.num2);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.num1 * answer.num2);
+}
+else if (answer.operator === "Divide") {
+    console.log(answer.num1 / answer.num2);
+}
+else {
+    console.log("Sorry Please Select your correct operator");
+}
+;
